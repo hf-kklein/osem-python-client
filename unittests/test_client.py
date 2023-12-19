35f3718b7e4fc3da3b38032c0bc0e1709861d345
konstantin <konstantin.klein@hochfrequenz.de>
2023-12-19 11:57:31 +0100

    Add method to associate sensor metadata with measurements
    and fix wrong datetime format string

--- unittests/test_client.py
+++ unittests/test_client.py
@@ -142,11 +142,11 @@ class TestClient:
             assert sensebox.id == "621f53cdb527de001b06ad5e"
             assert len(sensebox.sensors) == 11
 
-    async def test_get_data_from_sensor(self):
+    async def test_get_sensor_measurements(self):
         with aioresponses() as mocked_api:
             mocked_api.get(
                 # pylint:disable=line-too-long
-                "https://api.opensensemap.org/boxes/621f53cdb527de001b06ad5e/data/621f53cdb527de001b06ad69?format=json&from-date=2023-12-15T08-00-00.000000Z&to-date=2023-12-15T08-05-00.000000Z",
+                "https://api.opensensemap.org/boxes/621f53cdb527de001b06ad5e/data/621f53cdb527de001b06ad69?format=json&from-date=2023-12-15T08:00:00.000000Z&to-date=2023-12-15T08:05:00.000000Z",
                 status=200,
                 payload=[
                     {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:04:42.215Z", "value": "2.63"},
@@ -156,12 +156,98 @@ class TestClient:
                     {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:00:36.615Z", "value": "2.68"},
                 ],
             )
-
             client = OpenSenseMapClient()
-            measurements = await client.get_measurements(
+            measurements = await client.get_sensor_measurements(
                 "621f53cdb527de001b06ad5e",
                 "621f53cdb527de001b06ad69",
                 from_date=_berlin.localize(datetime(2023, 12, 15, 9, 0, 0, 0)),
                 to_date=_berlin.localize(datetime(2023, 12, 15, 9, 5, 0, 0)),
             )
             assert len(measurements) == 5
+
+    async def test_get_measurements_with_sensor_metadata(self):
+        with aioresponses() as mocked_api:
+            mocked_api.get(
+                "https://api.opensensemap.org/boxes/621f53cdb527de001b06ad5e",
+                status=200,
+                payload={
+                    "_id": "621f53cdb527de001b06ad5e",
+                    "createdAt": "2022-03-02T11:23:57.505Z",
+                    "updatedAt": "2023-12-18T13:06:48.041Z",
+                    "name": "CampusJahnallee",
+                    "currentLocation": {
+                        "timestamp": "2022-03-02T11:23:57.500Z",
+                        "coordinates": [12.353332, 51.340222],
+                        "type": "Point",
+                    },
+                    "exposure": "outdoor",
+                    "sensors": [
+                        {
+                            "title": "Temperatur",
+                            "unit": "°C",
+                            "sensorType": "HDC1080",
+                            "icon": "osem-thermometer",
+                            "_id": "621f53cdb527de001b06ad69",
+                            "lastMeasurement": {"createdAt": "2023-12-18T13:06:48.018Z", "value": "9.65"},
+                        },
+                        {
+                            "title": "rel. Luftfeuchte",
+                            "unit": "%",
+                            "sensorType": "HDC1080",
+                            "icon": "osem-humidity",
+                            "_id": "621f53cdb527de001b06ad68",
+                            "lastMeasurement": {"createdAt": "2023-12-18T13:06:48.018Z", "value": "74.66"},
+                        },
+                        # other sensors omitted for brevity
+                    ],
+                    "model": "homeV2EthernetFeinstaub",
+                    "lastMeasurementAt": "2023-12-18T13:06:48.018Z",
+                    "description": "SenseBox der GSD Sachunterricht unter besonderer Berücksichtigung von ...",
+                    "image": "621f53cdb527de001b06ad5e_rrlwm9.jpg",
+                    "weblink": "https://www.erzwiss.uni-leipzig.de/institut-fuer-paedagogik-und-didaktik-im-...",
+                    "loc": [
+                        {
+                            "geometry": {
+                                "timestamp": "2022-03-02T11:23:57.500Z",
+                                "coordinates": [12.353332, 51.340222],
+                                "type": "Point",
+                            },
+                            "type": "Feature",
+                        }
+                    ],
+                },
+            )
+            mocked_api.get(
+                # pylint:disable=line-too-long
+                "https://api.opensensemap.org/boxes/621f53cdb527de001b06ad5e/data/621f53cdb527de001b06ad69?format=json&from-date=2023-12-15T08:00:00.000000Z&to-date=2023-12-15T08:05:00.000000Z",
+                status=200,
+                payload=[
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:04:42.215Z", "value": "2.63"},
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:03:40.855Z", "value": "2.61"},
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:02:39.403Z", "value": "2.61"},
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:01:37.957Z", "value": "2.63"},
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:00:36.615Z", "value": "2.68"},
+                ],
+            )
+            mocked_api.get(
+                # pylint:disable=line-too-long
+                "https://api.opensensemap.org/boxes/621f53cdb527de001b06ad5e/data/621f53cdb527de001b06ad68?format=json&from-date=2023-12-15T08:00:00.000000Z&to-date=2023-12-15T08:05:00.000000Z",
+                status=200,
+                payload=[
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:04:42.215Z", "value": "99.51"},
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:03:40.855Z", "value": "99.61"},
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:02:39.403Z", "value": "99.65"},
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:01:37.957Z", "value": "99.71"},
+                    {"location": [12.353332, 51.340222], "createdAt": "2023-12-15T08:00:36.615Z", "value": "99.92"},
+                ],
+            )
+            client = OpenSenseMapClient()
+            results = await client.get_measurements_with_sensor_metadata(
+                "621f53cdb527de001b06ad5e",
+                from_date=_berlin.localize(datetime(2023, 12, 15, 9, 0, 0, 0)),
+                to_date=_berlin.localize(datetime(2023, 12, 15, 9, 5, 0, 0)),
+            )
+            assert len(results) == 10
+            # assert the correct sensors are associated with their respective data
+            assert all(3 > float(x.value) > 2 for x in results if x.unit == "°C")
+            assert all(100 > float(x.value) > 99 for x in results if x.unit == "%")
